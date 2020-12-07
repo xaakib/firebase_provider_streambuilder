ae004944226a46b6c0fb79aa1c8a0690a15eace3
Xaakib <64243974+xaakib@users.noreply.github.com>
2020-12-07 21:47:32 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# firebase_provider_streambuilder
+ firebase_provider_streambuilder ... data retive
---
+++ android/app/src/main/kotlin/com/example/firebase_provider_streambuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_provider_streambuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
