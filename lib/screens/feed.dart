7552d82bd221a1920f70834c3257c1618e8f8ce4
Xaakib <64243974+xaakib@users.noreply.github.com>
2020-12-24 17:57:34 +0600

    home ui done

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -32,51 +32,62 @@ class _FeedState extends State<Feed> {
 
     print("building Feed");
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          authNotifier.user != null ? authNotifier.user.displayName : "Feed",
-        ),
-        actions: <Widget>[
-          // action button
-          FlatButton(
-            onPressed: () => signout(authNotifier),
-            child: Text(
-              "Logout",
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-      body: new RefreshIndicator(
-        child: ListView.separated(
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              leading: Image.network(
-                foodNotifier.foodList[index].imageurl != null
-                    ? foodNotifier.foodList[index].imageurl
-                    : 'https://www.testingxperts.com/wp-content/uploads/2019/02/placeholder-img.jpg',
-                width: 120,
-                fit: BoxFit.fitWidth,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            elevation: 5,
+            expandedHeight: 200,
+            // flexibleSpace: Image.asset(
+            //   "assets/images/name.png",
+            //   fit: BoxFit.cover,
+            // ),
+            actions: [
+              Container(
+                height: 80,
+                width: 80,
+                child: Text(authNotifier.user != null
+                    ? authNotifier.user.displayName
+                    : "Unknown"),
               ),
-              title: Text(foodNotifier.foodList[index].name),
-              subtitle: Text(foodNotifier.foodList[index].category),
-              onTap: () {
-                foodNotifier.currentFood = foodNotifier.foodList[index];
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (BuildContext context) {
-                  return FoodDetail();
-                }));
-              },
-            );
-          },
-          itemCount: foodNotifier.foodList.length,
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              color: Colors.black,
-            );
-          },
-        ),
-        onRefresh: _refreshList,
+              IconButton(
+                  icon: Icon(
+                    Icons.people,
+                    size: 28,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (context) => UploadData()));
+                  }),
+            ],
+            floating: true,
+            leading: IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            title: Text(
+              "Movie Server",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+          ),
+          SliverPadding(padding: EdgeInsets.only(top: 20)),
+          SliverToBoxAdapter(
+            child: opulerContainerRow(foodNotifier),
+          ),
+          // SliverPadding(padding: EdgeInsets.only(top: 20)),
+          // SliverToBoxAdapter(
+          //   child: orangeContainerRow(orangeNotifier),
+          // ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -94,4 +105,157 @@ class _FeedState extends State<Feed> {
       ),
     );
   }
+
+  Widget opulerContainerRow(
+    FoodNotifier foodNotifier,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            "Food",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          height: 238,
+          child: ListView.builder(
+            itemCount: foodNotifier.foodList.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  foodNotifier.currentFood = foodNotifier.foodList[index];
+                  Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                    return FoodDetail();
+                  }));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 180,
+                          width: 135,
+                          child: Image.network(
+                            foodNotifier.foodList[index].imageurl.toString(),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 1),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                foodNotifier.foodList[index].name.toString(),
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 2,
+                              ),
+                              Text((foodNotifier.foodList[index].category
+                                  .toString())),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+//   Widget orangeContainerRow(OrangeNotifier orangeNotifier) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.only(left: 10),
+//           child: Text(
+//             "Orange",
+//             style: TextStyle(
+//               fontSize: 22,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//         ),
+//         Container(
+//           height: 238,
+//           child: ListView.builder(
+//             scrollDirection: Axis.horizontal,
+//             itemCount: orangeNotifier.orangeList.length,
+//             itemBuilder: (BuildContext context, int index) {
+//               return GestureDetector(
+//                 onTap: () {
+//                   orangeNotifier.currentOrange =
+//                       orangeNotifier.orangeList[index];
+//                   Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+//                     return FoodDetail();
+//                   }));
+//                 },
+//                 child: Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 8),
+//                   child: Card(
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Container(
+//                           height: 180,
+//                           width: 135,
+//                           child: Image.network(
+//                             orangeNotifier.orangeList[index].imageUrl
+//                                 .toString(),
+//                             fit: BoxFit.cover,
+//                           ),
+//                         ),
+//                         Padding(
+//                           padding: const EdgeInsets.only(left: 1),
+//                           child: Column(
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               Text(
+//                                 orangeNotifier.orangeList[index].name
+//                                     .toString(),
+//                                 style: TextStyle(
+//                                   fontSize: 17,
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                               SizedBox(
+//                                 height: 2,
+//                               ),
+//                               Text((orangeNotifier.orangeList[index].category
+//                                   .toString())),
+//                             ],
+//                           ),
+//                         )
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
 }
